12f39484f0ee66ef177273ca31bba8f2fd155150
Matanyhao <83552555+Matanyhao@users.noreply.github.com>
2023-02-12 18:52:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #pip install uvicorn
 #pip install yfinance
 #pip install finnhub-python
+#pip install js.jquery
 
 
 import models
@@ -33,10 +34,14 @@ def get_db():
 
 
 @app.get("/")
-def home(request: Request):
+def home(request: Request, db: Session = Depends(get_db)):
+
+    stocks = db.query(Stock).all()
+
     #stock screen dashboard - Homepage
     return templates.TemplateResponse("home.html", {
-        "request": request
+        "request": request,
+        "stocks": stocks
     })
 
 def fetch_stock_data(id: int):
@@ -47,13 +52,18 @@ def fetch_stock_data(id: int):
     finnhub_client = finnhub.Client(api_key="cea6leaad3i831op8bv0cea6leaad3i831op8bvg")
     n = stock.symbol
     X_price = (finnhub_client.quote(n))
+    print(X_price)
+    stock.openprice = X_price["o"]
     stock.lastprice = X_price["c"]
-
-
+    stock.lowprice = X_price["l"]
+    stock.highprice = X_price["h"]
+    stock.changeusd = X_price["d"]
+    stock.changeprecent = X_price["dp"]
 
 
     db.add(stock)
     db.commit()
+
 
 
 @app.post("/stock")
@@ -62,7 +72,6 @@ async def create_stock(stock_request: StockRequest, background_tasks: Background
 
     stock = Stock()
     stock.symbol = stock_request.symbol
-
     db.add(stock)
     db.commit()
 
@@ -72,3 +81,4 @@ async def create_stock(stock_request: StockRequest, background_tasks: Background
         "code": "success",
         "message": "stock created"
     }
+
--- models.py
+++ models.py
@@ -8,8 +8,11 @@ class Stock(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     symbol = Column(String, unique=True, index=True)
-    numofshares = Column(Numeric(10, 2))
-    avgprice = Column(Numeric(10, 2))
+    openprice = Column(Numeric(10, 2))
     lastprice = Column(Numeric(10, 2))
+    lowprice = Column(Numeric(10, 2))
+    highprice = Column(Numeric(10, 2))
+    changeusd = Column(Numeric(10, 2))
+    changeprecent = Column(Numeric(10, 2))
 
 
